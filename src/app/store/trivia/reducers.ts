35201d7597805ffe7cf843d8c1a916dbdca51f17
Flávio Barcelos <flavio.barcelos@shieldfc.com>
2023-07-06 17:34:28 +0100

    fix store

--- src/app/store/trivia/reducers.ts
+++ src/app/store/trivia/reducers.ts
@@ -8,40 +8,39 @@ import {
   GET_ALL_TRIVIA_FAILURE,
   GET_ALL_TRIVIA_SUCCESS
 } from "./actions";
-import {shibaTrivia} from "../../utils";
 
 export const triviaStateFeatureKey = 'triviaState';
 
 export interface TriviaState {
-  trivia: Trivia[];
-  isLoading: boolean;
-  errors: Error[];
+    trivia: Trivia[];
+    isLoading: boolean;
+    errors: Error[];
 }
 
 const initialTriviaState: TriviaState = {
-  trivia: [shibaTrivia],
-  isLoading: false,
-  errors: []
+    trivia: [],
+    isLoading: false,
+    errors: []
 
 }
 
 export const triviaReducer = createReducer(
-  initialTriviaState,
-  on(ADD_TRIVIA, GET_ALL_TRIVIA, state => ({...state, isLoading: true})),
-  on(ADD_TRIVIA_FAILURE, GET_ALL_TRIVIA_FAILURE, (state, {payload}) => ({
-    ...state,
-    isLoading: false,
-    errors: [...state.errors, payload]
-  })),
-  on(ADD_TRIVIA_SUCCESS, (state, {payload}) => ({
-    ...state,
-    isLoading: false,
-    trivia: [...state.trivia, payload]
-  })),
-  on(GET_ALL_TRIVIA_SUCCESS, (state, {payload}) => ({
-      ...state,
-      isLoading: false,
-      trivia: payload
-    }),
-  )
+    initialTriviaState,
+    on(ADD_TRIVIA, GET_ALL_TRIVIA, state => ({...state, isLoading: true})),
+    on(ADD_TRIVIA_FAILURE, GET_ALL_TRIVIA_FAILURE, (state, {payload}) => ({
+        ...state,
+        isLoading: false,
+        errors: [...state.errors, payload]
+    })),
+    on(ADD_TRIVIA_SUCCESS, (state, {payload}) => ({
+        ...state,
+        isLoading: false,
+        trivia: [...state.trivia, payload]
+    })),
+    on(GET_ALL_TRIVIA_SUCCESS, (state, {payload}) => ({
+            ...state,
+            isLoading: false,
+            trivia: payload
+        }),
+    )
 );
